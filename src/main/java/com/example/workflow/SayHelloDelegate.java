60a3297f8229d52370e34d62d2b89a87faa52457
linh <fastrocket@gmail.com>
2021-05-25 04:16:55 -0600

    Added SkipIt task and delegate; and expression to control flow

--- src/main/java/com/example/workflow/SayHelloDelegate.java
+++ src/main/java/com/example/workflow/SayHelloDelegate.java
@@ -5,6 +5,8 @@ import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 @Component
 public class SayHelloDelegate implements JavaDelegate {
     private GenericSender sender;
@@ -16,11 +18,18 @@ public class SayHelloDelegate implements JavaDelegate {
 
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
+        Random rand = new Random();
+
         String helloText = "Now is the time!!!";
         String text = "SayHelloDelegate setting variable hello: " + helloText;
         delegateExecution.setVariable("hello", text);
         System.out.println(text);
         System.out.println("SayHelloDelegate sending to Kafka text: " + text);
         sender.send(text);
+
+        // Randomly pick a path
+        boolean doIt = rand.nextBoolean();
+        System.out.println("SayHelloDelegate setting doIt=" + doIt);
+        delegateExecution.setVariable("doIt", doIt);
     }
 }
--- src/main/java/com/example/workflow/SkipItDelegate.java
+++ src/main/java/com/example/workflow/SkipItDelegate.java
@@ -4,13 +4,13 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Component;
 
-@Component(value = "MakeItSoDelegate")
-public class MakeItSoDelegate implements JavaDelegate {
+@Component(value = "SkipItDelegate")
+public class SkipItDelegate implements JavaDelegate {
 
     @Override
     public void execute(final DelegateExecution delegateExecution) {
         boolean doIt = (boolean)delegateExecution.getVariable("doIt");
         String myVar = (String)delegateExecution.getVariable("hello");
-        System.out.println("!!!!! MakeItSoDelegate got variable 'hello' with value: " + myVar + " and doIt: " + doIt);
+        System.out.println("!!!!! SkipItDelegate got variable 'hello' with value: " + myVar + " and doIt: " + doIt);
     }
 }
