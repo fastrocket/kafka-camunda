57b1c78013eb025c7ff7438ab2dbb8bcf2322ffb
Linh Ngo <linh@binaryhelix.com>
2021-12-25 13:52:51 -0700

    random uuid key

--- src/main/java/com/example/workflow/GenericReceiver.java
+++ src/main/java/com/example/workflow/GenericReceiver.java
@@ -1,18 +1,20 @@
 package com.example.workflow;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
-
-import static org.slf4j.LoggerFactory.getLogger;
 
 @Slf4j
 @Service
 public class GenericReceiver {
 
     @KafkaListener(topics = "${kafka.topic}")
-    public void receive(String message) {
-        log.info("received payload='{}'", message);
+    public void receive(@Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+                        @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String key,
+                        @Payload String message) {
+        log.info("received topic={} key={} payload='{}'", topic, key, message);
     }
 }
--- src/main/java/com/example/workflow/GenericSender.java
+++ src/main/java/com/example/workflow/GenericSender.java
@@ -3,11 +3,12 @@ package com.example.workflow;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-import static org.slf4j.LoggerFactory.getLogger;
+import java.util.UUID;
 
-import org.springframework.kafka.core.KafkaTemplate;
+import static org.slf4j.LoggerFactory.getLogger;
 
 @Service
 public class GenericSender {
@@ -21,13 +22,15 @@ public class GenericSender {
     private KafkaTemplate<String, String> kafkaTemplate;
 
     public void send(String topic, String payload) {
-        LOGGER.info("sending payload='{}' to topic='{}'", payload, topic);
-        kafkaTemplate.send(topic, payload);
+        String key = UUID.randomUUID().toString();
+        LOGGER.info("sending payload='{}' to topic='{}' with key={}", payload, topic, key);
+        kafkaTemplate.send(topic, key, payload);
 
     }
 
     public void send(String payload) {
-        LOGGER.info("sending payload='{}' to topic='{}'", payload, topic);
-        kafkaTemplate.send(this.topic, payload);
+        String key = UUID.randomUUID().toString();
+        LOGGER.info("sending payload='{}' to topic='{}' with key={}", payload, topic, key);
+        kafkaTemplate.send(this.topic, key, payload);
     }
 }
