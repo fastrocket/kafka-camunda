6ccdbf3fc385e0ad3f8e944fa5436e369121d8c2
Linh Ngo <linh@binaryhelix.com>
2021-12-25 09:47:51 -0700

    docker compose config6 bitnami

--- src/main/java/com/example/workflow/TopicConfig.java
+++ src/main/java/com/example/workflow/TopicConfig.java
@@ -1,13 +1,29 @@
 package com.example.workflow;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class TopicConfig {
     public static final String USER_TOPIC = "users";
+
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        return new KafkaAdmin(configs);
+    }
 
     @Bean
     public NewTopic createUsersTopic() {
